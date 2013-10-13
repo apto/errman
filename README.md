72b0b02488364f9bd1dcd5f27950a8591da76453
Justin Deal <justin.deal@gmail.com>
2013-10-12 20:29:17 -0500

    errman can now make new errman scopes

--- README.md
+++ README.md
@@ -6,6 +6,8 @@ errman
 errman is an error helper that lets you:
 
 - Create custom error types that extend from Error and have a proper stacktrace.
+- Register your custom error types with a default error registry, or create a
+  new errman instance to scope your error registry.
 - Use mustache-style templates to set a common error message for all instances
   of your error type. Supply an options object with your new error, and the
   key/value pairs are used in your message template. This way, you can have
@@ -27,32 +29,32 @@ component install errman
 
 ## Usage
 
-### Register an error type
+### Create an error type
 
 ```js
 var errman = require('errman');
 
 // Create a type with no message template and status code of 500.
-errman.type('BadMojoError');
+var BadMojoError = errman.type('BadMojoError');
 
 // You can pass a string to your custom error constructor to just set the error
 // message old school.
-throw new errman.BadMojoError('No way, dude!');
+throw new BadMojoError('No way, dude!');
 ```
 
-### Register an error type with an error template and status code
+### Create an error type with an error template and status code
 
 ```js
 var errman = require('errman');
 
 // Use mustache template for error message, and set status code to 404.
-errman.type('FileNotFoundError', {
+var FileNotFoundError = errman.type('FileNotFoundError', {
   message: 'Could not find file: {{filePath}}',
   status: 404
 });
 
 try {
-  throw new errman.FileNotFoundError({filePath: '/some/file/path.txt'}); 
+  throw new FileNotFoundError({filePath: '/some/file/path.txt'}); 
 } catch (e) {
   console.log(e.message);
 }
@@ -62,4 +64,42 @@ The error message printed to the console will be:
 
 ```plain
 Could not find file: /some/file/path.txt
+```
+
+### Register an error type
+
+```js
+var errman = require('errman');
+
+errman.registerType('BadMojoError');
+errman.registerType('FileNotFoundError', {
+  message: 'Could not find file: {{filePath}}',
+  status: 404
+});
+
+// Registered errors are available on the errman module.
+throw new errman.BadMojoError;
+throw new errman.FileNotFoundError({filePath: '/some/file/path.txt'}); 
+```
+
+### Create a new errman scope
+
+```js
+var errman = require('errman');
+
+errman.registerType('BadMojoError');
+
+var myErrMan = errman();
+
+myErrMan.registerType('FileNotFoundError', {
+  message: 'Could not find file: {{filePath}}',
+  status: 404
+});
+
+throw new errman.BadMojoError;
+throw new myErrMan.FileNotFoundError({filePath: '/some/file/path.txt'});
+// This won't work:
+throw new myErrMan.BadMojoError;
+// Neither will this:
+throw new errMan.FileNotFoundError({filePath: '/some/file/path.txt'});
 ```
--- index.js
+++ index.js
@@ -1,21 +1,31 @@
 var minstache = require('minstache');
 
+var UnknownError;
+
 var toJSON = function (err) {
   err = err || this;
   if (!(err instanceof Error)) {
-    err = new exports.UnknownError(err);
+    err = new UnknownError(err);
   }
   return {
     status: err.status || 500,
-    code: err.code || err.name,
-    message: err.message,
+    code: err.code || err.name || 'unknown',
+    message: err.message || '',
     detail: err.detail || {},
     stack: err.stack
   };
 };
 
-var createErrorType = function (name, options) {
-  var constructor = function (detail) {
+var ErrorManager = function (fn) {
+  this.types = fn;
+};
+
+var proto = ErrorManager.prototype;
+
+proto.toJSON = toJSON;
+
+var errorConstructor = function (name, options) {
+  return function (detail) {
     var self = this;
     if (Error.captureStackTrace) {
       Error.captureStackTrace(self, self.constructor || self);
@@ -30,28 +40,56 @@ var createErrorType = function (name, options) {
         message = minstache(options.message, detail);
       }
     }
+    name = name || 'unknown';
+    message = message || '';
+    detail = detail || {};
     self.message = message;
     self.status = options.status || 500;
     self.code = options.code || name;
     self.detail = detail;
     self.name = name;
   };
-  exports[name] = constructor;
+};
+
+proto.type = function (name, options) {
+  var self = this;
+  if (typeof name === 'object') {
+    options = name;
+    name = options.code;
+  }
+  options = options || {};
+  var constructor = errorConstructor(name, options);
   constructor.prototype = new Error();
   constructor.prototype.constructor = constructor;
   constructor.prototype.toJSON = toJSON;
   constructor.options = options;
+  constructor.typeName = name;
   return constructor;
 };
 
-var errorType = function (typeName, options) {
-  options = options || {};
-  exports[typeName] = createErrorType(typeName, options);
+proto.registerType = function (fn, name, options) {
+  if (typeof fn === 'string') {
+    options = name;
+    name = fn;
+    var errorType = this.type(name, options);
+    return this.registerType(errorType);
+  }
+  this.types[name || fn.typeName] = fn;
 };
 
-exports.type = errorType;
-exports.toJSON = toJSON;
+var createErrorManager = function () {
+  var errorManagerFn = function () {
+    return createErrorManager();
+  };
+  var errorManager = new ErrorManager(errorManagerFn);
+  Object.keys(proto).forEach(function (key) {
+    errorManagerFn[key] = proto[key].bind(errorManager);
+  });
+  return errorManagerFn;
+};
 
-errorType('UnknownError', {
-  code: 'unknown_error'
-});
+var defaultErrorManager = createErrorManager();
+
+UnknownError = defaultErrorManager.type('UnknownError');
+
+module.exports = defaultErrorManager;
